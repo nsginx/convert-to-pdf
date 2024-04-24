5141d860670c1ac7982eeb171db7dce258f747c2
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2024-04-25 01:12:11 +0530

    test function call removed

--- src/helpers/FetchDataArray.js
+++ src/helpers/FetchDataArray.js
@@ -35,11 +35,11 @@ export default async function fetchDataArray(token, level, places, state, timefr
 
 //test function
 
-async function test(){
-    const response= await fetchDataArray("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWViMDk3YTA4MjI5YWUyYzZlN2I0MWYiLCJmaXJzdE5hbWUiOiJTb3Vyb2RlZXAiLCJsYXN0TmFtZSI6IkFjaGFyeWEiLCJlbWFpbCI6ImFjaGFyeWFzb3Vyb2RlZXBAZ21haWwuY29tIiwicm9sZXMiOlsiYmFzaWMiXSwiZGVzaWduYXRpb24iOiJJbnRlcm4iLCJjb21wYW55TmFtZSI6IkRTIiwiaXNBdXRob3JpemVkIjp0cnVlLCJwYXNzd29yZCI6bnVsbCwiY3JlYXRlZEF0IjoiMjAyNC0wMy0wOFQxMDoyMDo0OC4yMDNaIiwidXBkYXRlZEF0IjoiMjAyNC0wMy0wOFQxMDoyMDo0OC4yMDNaIiwidGVhbUlEIjoiNjVlYjA4MWEwODIyOWFlMmM2ZTdiNDE5IiwiY3JlYXRlZEJ5IjpudWxsLCJ1cGRhdGVkQnkiOm51bGwsInR5cGUiOiJ0b2tlbiIsImlhdCI6MTcxMDIzOTM3NiwiZXhwIjo0NzEwMjM5Mzc2fQ.Ys5wtgqJeSHY7nQRwKuFrnHaRwp-19K5JvpJK6lfjfE", "pincode", [700001], "west bengal", ["2023-2024_Q2"], ["Company","Partnership"], ["Slab: Rs. 25 Cr. to 100 Cr."], ["Kirana_store"], ["AL", "HL"], ["public", "private", "nbfc", "foreign"],["2L-4L", "4L-6L" ,"15L-40L" ,"1Cr-2Cr"], ["private"] , false, true);
-    console.log("hello");
-    console.log(response[0]);
+// async function test(){
+//     const response= await fetchDataArray("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWViMDk3YTA4MjI5YWUyYzZlN2I0MWYiLCJmaXJzdE5hbWUiOiJTb3Vyb2RlZXAiLCJsYXN0TmFtZSI6IkFjaGFyeWEiLCJlbWFpbCI6ImFjaGFyeWFzb3Vyb2RlZXBAZ21haWwuY29tIiwicm9sZXMiOlsiYmFzaWMiXSwiZGVzaWduYXRpb24iOiJJbnRlcm4iLCJjb21wYW55TmFtZSI6IkRTIiwiaXNBdXRob3JpemVkIjp0cnVlLCJwYXNzd29yZCI6bnVsbCwiY3JlYXRlZEF0IjoiMjAyNC0wMy0wOFQxMDoyMDo0OC4yMDNaIiwidXBkYXRlZEF0IjoiMjAyNC0wMy0wOFQxMDoyMDo0OC4yMDNaIiwidGVhbUlEIjoiNjVlYjA4MWEwODIyOWFlMmM2ZTdiNDE5IiwiY3JlYXRlZEJ5IjpudWxsLCJ1cGRhdGVkQnkiOm51bGwsInR5cGUiOiJ0b2tlbiIsImlhdCI6MTcxMDIzOTM3NiwiZXhwIjo0NzEwMjM5Mzc2fQ.Ys5wtgqJeSHY7nQRwKuFrnHaRwp-19K5JvpJK6lfjfE", "pincode", [700001], "west bengal", ["2023-2024_Q2"], ["Company","Partnership"], ["Slab: Rs. 25 Cr. to 100 Cr."], ["Kirana_store"], ["AL", "HL"], ["public", "private", "nbfc", "foreign"],["2L-4L", "4L-6L" ,"15L-40L" ,"1Cr-2Cr"], ["private"] , false, true);
+//     console.log("hello");
+//     console.log(response[0]);
 
-}
+// }
 
-test();
+// test();
